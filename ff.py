de161db5600958daf2f5d8baaecf8589b5e8a5a9
terence <yangfancs1997@gmail.com>
2020-10-20 22:01:57 +0800

    add class

--- ff.py
+++ ff.py
@@ -44,3 +44,41 @@ def bar():
     print ('bar')
 
 bar()
+
+# class Person(object):
+#     def talk(self):
+#         print('person is talking......')
+#
+# class Chinese(Person):
+#     def walk(self):
+#         print('is walking...')
+#
+# c = Chinese()
+# c.talk()
+# c.walk()
+#
+
+class Person(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.weight = 'weight'
+
+    def talk(self):
+        print('person is talking...')
+
+class Chinese(Person):
+    def __init__(self, name, age, language):
+        Person.__init__(self, name, age)
+        self.language = language
+        print(self.name, self.age, self.weight, self.language)
+    def talk(self):
+        print('%s is speaking chinese' % self.name)
+    def walk(self):
+        print('is walking...')
+
+class American(Person):
+    pass
+
+c = Chinese('bigberg', 22, 'Chinese')
+c.talk()
